326a0d00b473b3f00d0e17dd62b170c8f713dd9d
Dmytro Zykov <dmytri.zykov@gmail.com>
2025-08-26 17:56:10 +0300

    Fix error alert

--- MovieTracker/Utilities/Extensions/UIViewController+Ext.swift
+++ MovieTracker/Utilities/Extensions/UIViewController+Ext.swift
@@ -3,11 +3,15 @@ import UIKit
 extension UIViewController {
     @MainActor func presentErrorAlert(for error: any Error) {
         let title = NSLocalizedString("error_alert_title", comment: "Error alert title")
-        let alertController = UIAlertController(
+        let alert = UIAlertController(
             title: title,
             message: error.localizedDescription,
             preferredStyle: .alert
         )
-        present(alertController, animated: true)
+        alert.addAction(UIAlertAction(
+            title: NSLocalizedString("error_alert_default_action", comment: "Error alert default action"),
+            style: .default
+        ))
+        present(alert, animated: true)
     }
 }

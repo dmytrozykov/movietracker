efb035cdebc6bbf5f334ba7372b29da4f211669a
Dmytro Zykov <dmytri.zykov@gmail.com>
2025-08-28 22:53:53 +0300

    Slight refactoring of TMDbService

--- MovieTracker/Services/TMDbService.swift
+++ MovieTracker/Services/TMDbService.swift
@@ -25,34 +25,31 @@ final class TMDbService {
     }
 }
 
+// swiftlint:disable nesting
 extension TMDbService {
-    enum Endpoint {
+    private enum Endpoint {
         private static let baseUrl = "https://api.themoviedb.org/3"
+
+        enum MovieList {
+            static let base = "\(baseUrl)/movie"
+
+            static func popular(page: Int) -> String {
+                "\(base)/popular?language=\(Locale.current.bcp47Identifier)&page=\(page)"
+            }
+        }
+
+        enum Image {
+            static let base = "https://image.tmdb.org/t/p"
+
+            static func get(from path: String, size: TMDbImageSize = .original) -> String {
+                "\(base)/\(size.rawValue)\(path)"
+            }
+        }
     }
 }
+
+// swiftlint:enable nesting
 
 enum TMDbImageSize: String {
-    case w92
-    case w154
-    case w185
-    case w342
-    case w500
-    case w780
-    case original
-}
-
-extension TMDbService.Endpoint {
-    enum MovieList {
-        static let base = "\(baseUrl)/movie"
-        static func popular(page: Int) -> String {
-            "\(base)/popular?language=\(Locale.current.bcp47Identifier)&page=\(page)"
-        }
-    }
-
-    enum Image {
-        static let base = "https://image.tmdb.org/t/p"
-        static func get(from path: String, size: TMDbImageSize = .original) -> String {
-            "\(base)/\(size.rawValue)\(path)"
-        }
-    }
+    case w92, w154, w185, w342, w500, w780, original
 }
